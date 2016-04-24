0584e647fb8b4b6055cfa69b9c63a87bb22e7956
lonewolf <nikhilkalige@gmail.com>
2016-04-24 11:48:11 -0700

    Add torque control

--- manipulator_driver/scripts/test_joint_trajectory_action.py
+++ manipulator_driver/scripts/test_joint_trajectory_action.py
@@ -2,6 +2,7 @@
 import roslib
 import rospy
 import actionlib
+from manipulator_actions.msg import ControlTorqueGoal, ControlTorqueAction
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectoryPoint
@@ -21,6 +22,24 @@ def get_current_joint_angles():
     return positions
 
 
+def torque_control(enable=True):
+    client = actionlib.SimpleActionClient('control_torque', ControlTorqueAction)
+    print("Waiting for torque server")
+    client.wait_for_server()
+    print("Connected to torque server")
+
+    goal = ControlTorqueGoal()
+    goal.joint_names = joint_names
+    goal.enable = [enable] * len(joint_names)
+
+    client.send_goal(goal)
+    client.wait_for_result()
+
+    result = client.get_result()
+    print result
+    return result
+
+
 def test_joint_trajectory_action():
     time_from_start = 0  # in seconds
     time_between_pts = 1
@@ -29,7 +48,8 @@ def test_joint_trajectory_action():
 
     client = actionlib.SimpleActionClient(
         'pos_based_pos_traj_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
-    print('Waiting for server')
+
+    torque_control()
 
     def stop():
         client.cancel_goal()
@@ -39,7 +59,7 @@ def test_joint_trajectory_action():
     print("Connected to server")
 
     goal = FollowJointTrajectoryGoal()
-    rospy.on_shutdown(stop)
+    # rospy.on_shutdown(stop)
 
     goal.goal_time_tolerance = goal_time_tolerance
     goal.trajectory.joint_names = ['shoulder_pan_joint', 'shoulder_lift_joint',
@@ -48,8 +68,8 @@ def test_joint_trajectory_action():
     # current_pos = get_current_joint_angles()
     # print current_pos
     current_pos = [
-        0.0121557539874477, 1.5773061005648537, -0.7216586965606695,
-        0.00599650479916318, 0.07443444336635946, -0.16748266806912443
+        0.002078120661087866, 1.6091038504393307, -0.8027179211422595,
+        0.00599650479916318, -0.07228351752304148, -0.16890972463824888
     ]
     # First point is current position
     first_point = JointTrajectoryPoint()
@@ -71,7 +91,7 @@ def test_joint_trajectory_action():
         goal.trajectory.points.append(point)
 
     # Start trajectory after 2 seconds
-    goal.trajectory.header.stamp = rospy.Time.now()
+    # goal.trajectory.header.stamp = rospy.Time.now() + rospy.Duration(2)
     # print goal
     print("Time secs", goal.trajectory.header.stamp.to_sec())
     client.send_goal(goal)
@@ -83,6 +103,7 @@ def test_joint_trajectory_action():
         print("Hooray, the arm finished the trajectory!")
     else:
         print("The arm failed to execute the trajectory.")
+    print client.get_state()
     return client.get_result()
 
 
@@ -90,6 +111,7 @@ if __name__ == '__main__':
     try:
         rospy.init_node('joint_trajectory_test_client', log_level=rospy.DEBUG)
         result = test_joint_trajectory_action()
+        print result
         print("Finished execution")
     except rospy.ROSInterruptException:
         print("Program interrupted")
